9ebbc857e684b429a939511c858d2da6a9ee55eb
Timeraa <29104008+Timeraa@users.noreply.github.com>
2019-04-17 21:09:48 +0200

    ➕ Presence Management

---
+++ Extension/js/util/presenceManager.js
@@ -0,0 +1,48 @@
+function addPresence(name) {
+	chrome.storage.local.get('presences', async function(presences) {
+		presences = presences.presences;
+
+		if (presences.find((p) => p.service == name) != undefined) {
+			PMD_error('Presence already added');
+			return;
+		}
+
+		var json = await fetchJSON(`https://api.premid.app/presences/${name}`);
+
+		if (json.error) {
+			PMD_error(`Presence ${name} not found.`);
+			return;
+		}
+
+		var presenceMeta = await fetchJSON(json.url + 'metadata.json');
+
+		var presenceToAdd = {
+			service: name,
+			url: presenceMeta.url,
+			source: `${json.url}`,
+			color: presenceMeta.color,
+			enabled: true
+		};
+
+		if (presenceMeta.iframe) presenceToAdd.iframe = presenceToAdd.iframe;
+
+		presences.push(presenceToAdd);
+
+		chrome.storage.local.set({ presences: presences });
+	});
+}
+
+function removePresence(name) {
+	chrome.storage.local.get('presences', async function(presences) {
+		presences = presences.presences;
+
+		var presenceToRemove = presences.find((p) => p.service == name);
+
+		if (presenceToRemove == undefined) {
+			PMD_error('Presence not found');
+			return;
+		}
+
+		chrome.storage.local.set({ presences: presences.filter((p) => p.service != name) });
+	});
+}
