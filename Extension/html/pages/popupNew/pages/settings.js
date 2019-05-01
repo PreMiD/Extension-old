52c408d1c3740ab97af6714c0d49ae49d6a607ee
Timeraa <29104008+Timeraa@users.noreply.github.com>
2019-05-01 23:06:01 +0200

    ⚙️ Popup stuff

--- Extension/html/pages/popupNew/pages/settings.js
+++ Extension/html/pages/popupNew/pages/settings.js
@@ -7,7 +7,8 @@ Vue.component('settingsView', {
 				presences: ''
 			},
 			settings: {},
-			presences: []
+			presences: [],
+			thing: {}
 		};
 	},
 	created: async function() {
@@ -34,41 +35,50 @@ Vue.component('settingsView', {
 		//* Get presences, save in array
 		this.presences = await new Promise(function(resolve, reject) {
 			chrome.storage.local.get('presences', function(result) {
-				Promise.all(
-					result.presences.map((p) => {
-						p.color = `background-color: ${p.color}`;
-						return p;
-					})
-				).then((c) => resolve(c));
+				resolve(result.presences);
 			});
 		});
 	},
+	methods: {
+		updateSetting(key, value) {
+			chrome.storage.sync.get('settings', function(result) {
+				result.settings[key].value = value;
+
+				chrome.storage.sync.set(result);
+			});
+		},
+		updatePresence(key, value) {
+			chrome.storage.local.get('presences', function(result) {
+				result.presences.find((p) => p.service == key).enabled = value;
+
+				chrome.storage.local.set(result);
+			});
+		}
+	},
 	template: /*html*/ `
   <div id="settingsView">
-    <div id="general" class="panel">
-      <h1>{{this.strings.general}}</h1>
-      <div v-for="(value, key) in settings">
-				<h3>{{value.string}}</h3>
-				<div class="switch">
-					<label>
-						<input type="checkbox" checked="value.value">
-						<span class="lever"></span>
-					</label>
-				</div>
-      </div>
-    </div>
+		<v-container grid-list-md class="panel">
+			<h3>{{strings.general}}</h3>
+			<v-layout row wrap v-for="(value, key) in settings">
+				<v-flex xs17>
+					<h6>{{value.string}}</h6>
+				</v-flex>
+				<v-flex xs3>
+					<v-switch :input-value="value.value" color="#7289da" @change="updateSetting(key, $event)"></v-switch>
+				</v-flex>
+			</v-layout>
+		</v-container>
 
-    <div id="presences" class="panel">
-      <h1>{{this.strings.presences}}</h1>
-      <div v-for="(value, name) in presences">
-				<h3>{{value.service}}</h3>
-				<div class="switch">
-					<label>
-						<input type="checkbox" checked="value.enabled">
-						<span class="lever" :style="value.color"></span>
-					</label>
-				</div>
-      </div>
-    </div>
+		<v-container grid-list-md class="panel">
+			<h3>{{strings.presences}}</h3>
+			<v-layout row wrap v-for="(value, key) in presences">
+				<v-flex xs17>
+					<h6>{{value.service}} <p v-if="value.tmp">tmp</p></h6>
+				</v-flex>
+				<v-flex xs3>
+					<v-switch :input-value="value.enabled" :color="value.color" @change="updatePresence(value.service, $event)"></v-switch>
+				</v-flex>
+			</v-layout>
+		</v-container>
 	</div>`
 });
--- Extension/js/util/debug.js
+++ Extension/js/util/debug.js
@@ -1,11 +1,23 @@
 function PMD_info(message) {
-	console.log('%cPreMiD%c ' + message, 'color: #fff; font-weight: 900; padding: 3px; margin: 3px; background: #596cae;', 'color: #0000ff;');
+	console.log(
+		'%cPreMiD%c ' + message,
+		'color: #fff; font-weight: 900; padding: 3px; margin: 3px; background: #596cae; border-radius: 5px;',
+		'color: #0000ff;'
+	);
 }
 
 function PMD_error(message) {
-	console.log('%cPreMiD%c ' + message, 'color: #fff; font-weight: 900; padding: 3px; margin: 3px; background: #596cae;', 'color: #ff0000;');
+	console.log(
+		'%cPreMiD%c ' + message,
+		'color: #fff; font-weight: 900; padding: 3px; margin: 3px; background: #596cae; border-radius: 5px;',
+		'color: #ff0000;'
+	);
 }
 
 function PMD_success(message) {
-	console.log('%cPreMiD%c ' + message, 'color: #fff; font-weight: 900; padding: 3px; margin: 3px; background: #596cae;', 'color: #00ff00;');
+	console.log(
+		'%cPreMiD%c ' + message,
+		'color: #fff; font-weight: 900; padding: 3px; margin: 3px; background: #596cae; border-radius: 5px;',
+		'color: #00ff00;'
+	);
 }
