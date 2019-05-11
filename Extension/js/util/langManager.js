e8b2041c8177b5d1b0478c5abea84726d333f5fc
Timeraa <29104008+Timeraa@users.noreply.github.com>
2019-05-11 23:51:10 +0200

    ⚙️ Fix langManager errors

--- Extension/js/util/langManager.js
+++ Extension/js/util/langManager.js
@@ -1,63 +1,70 @@
 var defaultLanguage = null,
-	currLanguage = null;
+  currLanguage = null;
 
 //* Load languages if set
 loadLanguages();
 
 async function loadLanguages() {
-	return new Promise(function(resolve, reject) {
-		if (defaultLanguage == null) {
-			chrome.storage.sync.get('settings', function(res) {
-				if (res.settings == undefined) return;
+  return new Promise(function(resolve, reject) {
+    if (defaultLanguage == null) {
+      chrome.storage.sync.get("settings", function(res) {
+        if (res.settings == undefined) return;
 
-				chrome.storage.local.get('languages', function(res1) {
-					if (res1.languages == undefined) {
-						reject();
-						return;
-					}
-					PMD_info('Translations has been loaded.');
+        chrome.storage.local.get("languages", function(res1) {
+          if (res1.languages == undefined) {
+            reject();
+            return;
+          }
+          PMD_info("Translations has been loaded.");
 
-					resolve();
-					defaultLanguage = res1.languages.default;
-					currLanguage = res1.languages[res.settings.language.value];
-				});
-			});
-		} else resolve();
-	});
+          resolve();
+          defaultLanguage = res1.languages.default;
+          currLanguage =
+            res1.languages[res.settings.language.value.toLowerCase()];
+        });
+      });
+    } else resolve();
+  });
 }
 
 function updateLanguages() {
-	chrome.storage.sync.get('settings', async function(res) {
-		if (res.settings == undefined) return;
+  chrome.storage.sync.get("settings", async function(res) {
+    if (res.settings == undefined) return;
 
-		PMD_info(`Updating translations for en && ${res.settings.language.value.toLowerCase()}`);
-		defaultLanguage = await fetchJSON(`https://api.premid.app/langFile/en`);
-		var result = await fetchJSON(`https://api.premid.app/langFile/${res.settings.language.value.toLowerCase()}`);
+    PMD_info(
+      `Updating translations for en && ${res.settings.language.value.toLowerCase()}`
+    );
+    defaultLanguage = await fetchJSON(`https://api.premid.app/langFile/en`);
+    var result = await fetchJSON(
+      `https://api.premid.app/langFile/${res.settings.language.value.toLowerCase()}`
+    );
 
-		if (result.error != undefined) {
-			PMD_error(
-				`Failed to fetch language file by code: ${res.settings.language.value.toLowerCase()}, using default language instead`
-			);
-			currLanguage = defaultLanguage;
-			return;
-		} else currLanguage = result;
-
-		chrome.storage.local.set({
-			languages: {
-				default: defaultLanguage,
-				[res.settings.language.value.toLowerCase()]: currLanguage
-			}
-		});
-	});
+    if (result.error != undefined) {
+      PMD_error(
+        `Failed to fetch language file by code: ${res.settings.language.value.toLowerCase()}, using default language instead`
+      );
+      currLanguage = defaultLanguage;
+    } else currLanguage = result;
+    chrome.storage.local.set({
+      languages: {
+        default: defaultLanguage,
+        [res.settings.language.value.toLowerCase()]: currLanguage
+      }
+    });
+  });
 }
 
 async function getString(string) {
-	await loadLanguages();
-	if (currLanguage[string] != undefined) return currLanguage[string];
-	if (currLanguage[string] == undefined && defaultLanguage[string] != undefined) return;
-	console.log(defaultLanguage);
-	if (currLanguage[string] == undefined && defaultLanguage[string] == undefined) {
-		PMD_error(`No translation found for ${string}`);
-		return null;
-	}
+  await loadLanguages();
+  if (currLanguage[string] != undefined) return currLanguage[string];
+  if (currLanguage[string] == undefined && defaultLanguage[string] != undefined)
+    return;
+  console.log(defaultLanguage);
+  if (
+    currLanguage[string] == undefined &&
+    defaultLanguage[string] == undefined
+  ) {
+    PMD_error(`No translation found for ${string}`);
+    return null;
+  }
 }
