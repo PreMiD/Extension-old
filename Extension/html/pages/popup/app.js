3aac77d1d01ce03588b92e65bfa12433e63200fd
Timeraa <29104008+Timeraa@users.noreply.github.com>
2019-05-21 23:00:28 +0200

    ⚙️ Credits stuffs popup

--- Extension/html/pages/popup/app.js
+++ Extension/html/pages/popup/app.js
@@ -1,13 +1,13 @@
 //* Vue Router routes
 const routes = [
-	{ path: '*', redirect: '/settings' },
-	{ path: '/settings', component: Vue.component('settingsView') },
-	{ path: '/credits', component: Vue.component('creditsView') }
+  { path: "*", redirect: "/settings" },
+  { path: "/settings", component: Vue.component("settingsView") },
+  { path: "/credits", component: Vue.component("creditsView") }
 ];
 
 //* Vue Router
 const app = new Vue({
-	router: new VueRouter({
-		routes
-	})
-}).$mount('#app');
+  router: new VueRouter({
+    routes
+  })
+}).$mount("#app");
