97fbca57b7624d5c826d571b604e0851cef50702
Timeraa <metz.florian@arcor.de>
2019-06-29 15:04:20 +0200

    ♻️ Only addEventListeners to window if premid.app

--- Extension/js/util/presenceManager.js
+++ Extension/js/util/presenceManager.js
@@ -1,74 +1,84 @@
-window.addEventListener('PreMiD_AddPresence', function(data) {
-	addPresence(data.detail);
-});
+if (location.pathname !== "/_generated_background_page.html") {
+  window.addEventListener("PreMiD_AddPresence", function(data) {
+    addPresence(data.detail);
+  });
 
-window.addEventListener('PreMiD_RemovePresence', function(data) {
-	removePresence(data.detail);
-});
+  window.addEventListener("PreMiD_RemovePresence", function(data) {
+    removePresence(data.detail);
+  });
 
-window.addEventListener('PreMiD_GetPresenceList', function() {
-	console.log(
-		'%cPreMiD%c ' + 'Extension event call detected, sending data back...',
-		'color: #ffffff; font-weight: 900; padding: 3px; margin: 3px; background: #596cae;',
-		'color: #0000ff;'
-	);
-	chrome.storage.local.get('presences', (result) => {
-		var event = new CustomEvent('PreMiD_GetWebisteFallback', {
-			detail: result.presences.filter((p) => p.tmp == undefined).map((p) => p.service)
-		});
-		window.dispatchEvent(event);
+  window.addEventListener("PreMiD_GetPresenceList", function() {
+    console.log(
+      "%cPreMiD%c " + "Extension event call detected, sending data back...",
+      "color: #ffffff; font-weight: 900; padding: 3px; margin: 3px; background: #596cae;",
+      "color: #0000ff;"
+    );
+    chrome.storage.local.get("presences", result => {
+      var event = new CustomEvent("PreMiD_GetWebisteFallback", {
+        detail: result.presences
+          .filter(p => p.tmp == undefined)
+          .map(p => p.service)
+      });
+      window.dispatchEvent(event);
 
-		return;
-	});
-});
+      return;
+    });
+  });
+}
 
 function addPresence(name) {
-	chrome.storage.local.get('presences', async function(presences) {
-		presences = presences.presences;
+  chrome.storage.local.get("presences", async function(presences) {
+    presences = presences.presences;
 
-		if (presences.find((p) => p.service == name && p.tmp == undefined) != undefined) {
-			PMD_error('Presence already added');
-			return;
-		}
+    if (
+      presences.find(p => p.service == name && p.tmp == undefined) != undefined
+    ) {
+      PMD_error("Presence already added");
+      return;
+    }
 
-		var json = await fetchJSON(`https://api.premid.app/presences/${name}`);
+    var json = await fetchJSON(`https://api.premid.app/presences/${name}`);
 
-		if (json.error) {
-			PMD_error(`Presence ${name} not found.`);
-			return false;
-		}
+    if (json.error) {
+      PMD_error(`Presence ${name} not found.`);
+      return false;
+    }
 
-		var presenceMeta = await fetchJSON(json.url + 'metadata.json');
+    var presenceMeta = await fetchJSON(json.url + "metadata.json");
 
-		var presenceToAdd = {
-			service: name,
-			url: presenceMeta.url,
-			source: `${json.url}`,
-			color: presenceMeta.color,
-			enabled: true
-		};
+    var presenceToAdd = {
+      service: name,
+      url: presenceMeta.url,
+      source: `${json.url}`,
+      color: presenceMeta.color,
+      enabled: true
+    };
 
-		if (presenceMeta.iframe) presenceToAdd.iframe = presenceToAdd.iframe;
+    if (presenceMeta.iframe) presenceToAdd.iframe = presenceToAdd.iframe;
 
-		presences.push(presenceToAdd);
+    presences.push(presenceToAdd);
 
-		chrome.storage.local.set({ presences: presences });
-		PMD_info(`Presence ${name} was installed successfuly.`);
-	});
+    chrome.storage.local.set({ presences: presences });
+    PMD_info(`Presence ${name} was installed successfuly.`);
+  });
 }
 
 function removePresence(name) {
-	chrome.storage.local.get('presences', async function(presences) {
-		presences = presences.presences;
+  chrome.storage.local.get("presences", async function(presences) {
+    presences = presences.presences;
 
-		var presenceToRemove = presences.find((p) => p.service == name || p.tmp == undefined);
+    var presenceToRemove = presences.find(
+      p => p.service == name || p.tmp == undefined
+    );
 
-		if (presenceToRemove == undefined) {
-			PMD_error('Presence not found');
-			return false;
-		}
+    if (presenceToRemove == undefined) {
+      PMD_error("Presence not found");
+      return false;
+    }
 
-		chrome.storage.local.set({ presences: presences.filter((p) => p.service != name || p.tmp != undefined) });
-		PMD_info(`Presence ${name} was removed successfuly.`);
-	});
+    chrome.storage.local.set({
+      presences: presences.filter(p => p.service != name || p.tmp != undefined)
+    });
+    PMD_info(`Presence ${name} was removed successfuly.`);
+  });
 }
