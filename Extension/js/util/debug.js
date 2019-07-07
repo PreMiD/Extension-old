2282da86233bbae80acc27903f2203811b919609
Timeraa <metz.florian@arcor.de>
2019-07-08 00:13:42 +0200

    🎨 Just a nice touch to debug messages

--- Extension/js/util/debug.js
+++ Extension/js/util/debug.js
@@ -1,23 +1,29 @@
+var genericStyle = "font-weight: 800; padding: 2px 5px;";
+
 function PMD_info(message) {
-	console.log(
-		'%cPreMiD%c ' + message,
-		'color: #fff; font-weight: 900; padding: 3px 7px; margin: 3px; background: #596cae; border-radius: 50px;',
-		'color: #596cae;'
-	);
+  console.log(
+    "%cPreMiD%cINFO%c " + message,
+    genericStyle + "border-radius: 25px 0 0 25px; background: #596cae;",
+    genericStyle + "border-radius: 0 25px 25px 0; background: #5050ff;",
+    "color: white;"
+  );
 }
 
 function PMD_error(message) {
-	console.log(
-		'%cPreMiD%c ' + message,
-		'color: #fff; font-weight: 900; padding: 3px; margin: 3px; background: #596cae; border-radius: 5px;',
-		'color: #ff0000;'
-	);
+  console.log(
+    "%cPreMiD%cERROR%c " + message,
+    genericStyle + "border-radius: 25px 0 0 25px; background: #596cae;",
+    genericStyle + "border-radius: 0 25px 25px 0; background: #ff5050;",
+    "color: white;"
+  );
 }
 
 function PMD_success(message) {
-	console.log(
-		'%cPreMiD%c ' + message,
-		'color: #fff; font-weight: 900; padding: 3px; margin: 3px; background: #596cae; border-radius: 5px;',
-		'color: #00ff00;'
-	);
+  console.log(
+    "%cPreMiD%cSUCCESS%c " + message,
+    genericStyle + "border-radius: 25px 0 0 25px; background: #596cae;",
+    genericStyle +
+      "border-radius: 0 25px 25px 0; background: #50ff50; color: black;",
+    "color: white;"
+  );
 }
