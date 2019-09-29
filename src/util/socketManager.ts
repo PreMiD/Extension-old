d516733ff81a7316d726c9db8ebb89936e73c09b
Florian Metz <metz.florian@arcor.de>
2019-09-29 13:46:56 +0200

    🐛 Small bugfixing

--- src/util/socketManager.ts
+++ src/util/socketManager.ts
@@ -16,25 +16,32 @@ export function connect() {
 
 socket.on("connect", () => {
   socket.emit("getVersion");
+
+  let appVersion = setTimeout(() => {
+    chrome.storage.local.set({ appVersionSupported: false });
+    error("Unsupported app version");
+    socket.disconnect();
+  }, 1000);
   socket.once("receiveVersion", (version: number) => {
+    clearTimeout(appVersion);
     //TODO increase this for 2.0
-    if (version >= 202) {
+    if (version >= 203) {
       info("Supported app version");
       chrome.storage.local.set({ appVersionSupported: true });
     } else {
       chrome.storage.local.set({ appVersionSupported: false });
       error("Unsupported app version");
-      socket.disconnect();
+      return;
     }
+
+    if (oldActivity) setActivity(oldActivity);
+
+    success("Connected to application");
+    chrome.runtime.sendMessage({ socket: socket.connected });
+
+    if (priorityTab !== null)
+      chrome.tabs.sendMessage(priorityTab, { tabPriority: true });
   });
-
-  if (oldActivity) setActivity(oldActivity);
-
-  success("Connected to application");
-  chrome.runtime.sendMessage({ socket: socket.connected });
-
-  if (priorityTab !== null)
-    chrome.tabs.sendMessage(priorityTab, { tabPriority: true });
 });
 
 socket.on("disconnect", () => {
