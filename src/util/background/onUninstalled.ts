531507f963362ff2c76d0da8b9333f33ae37e1d5
alexbcberio <alexbcberio@gmail.com>
2020-05-16 21:24:33 +0200

    Open a tab to /v2/science to remove user from db onuninstall

---
+++ src/util/background/onUninstalled.ts
@@ -0,0 +1,7 @@
+import { apiBase } from "../../config";
+
+chrome.storage.local.get("identifier", s => {
+    if (s.identifier) {
+        chrome.runtime.setUninstallURL(`${apiBase}science/${s.identifier}/`);
+    }
+});
