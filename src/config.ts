cb2ba2d5592ca87470516d3a0f57e6751e4e77b0
Bas950 <34792838+Bas950@users.noreply.github.com>
2020-12-24 21:17:16 +0100

    :sparkles: Ready for release!

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const apiBase = "https://api.premid.app/v3/";
-export const releaseType: "BETA" | "ALPHA" | "RELEASE" | "DEV" = "DEV";
+export const releaseType: "BETA" | "ALPHA" | "RELEASE" | "DEV" = "RELEASE";
 export const requiredAppVersion = 214;
