43879d084cf63094397750553f212653365977de
xanx <78374441+xanxseven@users.noreply.github.com>
2022-11-18 16:02:10 +0100

    set exporting functions variable type to `const`
    i think axios or else is not getting modificated anywhere, so why it is as changeable variable?
    change it for more complex and easyness.

--- src/helper.ts
+++ src/helper.ts
@@ -4,8 +4,8 @@ import { apiBase as apibase } from "./config";
 import { getStorage as getstorage } from "./util/functions/asyncStorage";
 import { getString as getstring, getStrings as getstrings } from "./util/langManager";
 
-export let getString = getstring;
-export let getStrings = getstrings;
-export let getStorage = getstorage;
-export let axios = Axios;
-export let apiBase = apibase;
+export const getString = getstring;
+export const getStrings = getstrings;
+export const getStorage = getstorage;
+export const axios = Axios;
+export const apiBase = apibase;
